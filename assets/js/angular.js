e8f0831224d685c19b739f12cb0e21f515b0a51f
unknown <iamseyiajayi@gmail.com>
2019-09-13 08:38:55 +0100

    Added Favorites Page and Changed Page Titles

--- assets/js/angular.js
+++ assets/js/angular.js
@@ -11,4 +11,8 @@ app.controller('jobListsCtr', function($scope) {
             {image : 'https://ui-avatars.com/api/?background=f2f2f2&color=000&name=pm', title : 'Project Manager', company : 'Tripax Travel', location: 'Moscow', type : 'Remote', date : '5 days ago' }, 
   					{image : 'https://ui-avatars.com/api/?background=f2f2f2&color=000&name=t', title : 'Tester', company : 'Tripax Travel', location: 'Rio', type : 'Remote', date : 'Last Week' }, 
   				];
+  $scope.favorites = [
+            {image : 'https://ui-avatars.com/api/?background=f2f2f2&color=000&name=fd', title : 'Front-End Developer', company : 'Tripax Travel', location: 'Berlin', type : 'Remote', date : 'Today' }, 
+            {image : 'https://ui-avatars.com/api/?background=f2f2f2&color=000&name=cw', title : 'Creative Writer', company : 'Tripax Travel', location: 'Nairobi', type : 'Remote', date : 'Yesterday' }, 
+          ];
 });
